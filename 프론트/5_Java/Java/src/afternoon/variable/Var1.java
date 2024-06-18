6413538d30ebcc78b127e123b6281ae8c5f4f41a
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-06-18 17:06:59 +0900

    6월 18일 예제

---
+++ 프론트/5_Java/Java/src/afternoon/variable/Var1.java
@@ -0,0 +1,16 @@
+package afternoon.variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 4.5;
+        boolean c = true;
+        char d = 'c'; // 반드시 단 따옴표
+        String e = "Hello, JAVA World!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
