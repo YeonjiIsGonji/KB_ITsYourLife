f955ba691fd3cc51c60b59da1cefbb58bafe0dcd
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-06-25 17:44:06 +0900

    6월 25일 자바수업

---
+++ 프론트/5_Java/Java/src/afternoon/poly3/overriding/ex/Cat.java
@@ -0,0 +1,8 @@
+package afternoon.poly3.overriding.ex;
+
+public class Cat extends Animal{
+    @Override
+    public void sound() {
+        System.out.println("고양이는 야옹");
+    }
+}
---
+++ 프론트/5_Java/Java/src/afternoon/poly3/overriding/ex/Dog.java
@@ -0,0 +1,8 @@
+package afternoon.poly3.overriding.ex;
+
+public class Dog extends Animal{
+    @Override
+    public void sound() {
+        System.out.println("강아지가 멍멍");
+    }
+}
